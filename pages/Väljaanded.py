1ea152b296d8c9033d70a9a94f682fa7a843bf6b
Anri Sokolov <116002652+anriwv@users.noreply.github.com>
2024-02-10 00:01:56 +0200

    Update Väljaanded.py

--- pages/Väljaanded.py
+++ pages/Väljaanded.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-
 def embed_pdf(pdf_url):
     pdf_iframe = f'<iframe src="https://drive.google.com/viewerng/viewer?embedded=true&url={pdf_url}#toolbar=0&scrollbar=0" width="100%" height="800" style="border: none"></iframe>'
     return pdf_iframe
@@ -23,42 +22,35 @@ Täname, et jagate meiega huvi meie kooli pärandi vastu ning avastate koos meie
 """)
 
 
+
 # Establish connection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Function to read data from the selected sheet
-#@st.cache_data(ttl=2)  # Set the time-to-live (ttl) in seconds (e.g., 10 minutes)
+# Cache all data from the sheets
 @st.cache_data
-def read_data(selected_sheet_str):
-    return conn.read(worksheet=selected_sheet_str)
+def cache_all_data():
+    data = {}
+    list_data = conn.read(worksheet="LIST")
+    sheet_names = list_data.iloc[:, 0].dropna().astype(int).tolist()[::-1]
+    for sheet_name in sheet_names:
+        data[sheet_name] = conn.read(worksheet=str(sheet_name))
+    return data
 
-
-# Read the data from the "LIST" sheet to get the latest sheet names
-list_data = conn.read(worksheet="LIST", col="A")
-sheet_names = list_data.iloc[:, 0].dropna().astype(int).tolist()[::-1]
-
+# Read all data
+data_cache = cache_all_data()
 
 # Create a dropdown menu to choose the sheet
-selected_sheet = st.sidebar.selectbox("Select a sheet:", sheet_names)
-#selected_sheet = st.selectbox("Select a sheet:", sheet_names)
+selected_sheet = st.sidebar.selectbox("Select a sheet:", list(data_cache.keys()))
 
-
-
-# Convert the selected sheet value to a string and remove leading/trailing whitespaces
-selected_sheet_str = str(selected_sheet).strip()
-
-# Read the data from the selected sheet using the cached function
-selected_sheet_data = read_data(selected_sheet_str)
+# Read the data from the selected sheet
+selected_sheet_data = data_cache[selected_sheet]
 
 # Extract values from columns A and B, excluding NaN values
 names = selected_sheet_data.iloc[:, 0].dropna().tolist()
 pdf_urls = selected_sheet_data.iloc[:, 1].dropna().tolist()
 
-
-
 # Create tabs dynamically based on the names
 tabs = st.tabs(names)
-
 
 # Iterate through tabs to embed PDFs
 for i in range(min(len(names), len(pdf_urls))):
