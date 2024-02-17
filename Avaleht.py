70d17b7b0898d8d5d4f47884349feb152f567e14
Anri Sokolov <116002652+anriwv@users.noreply.github.com>
2024-02-17 20:10:05 +0200

    Update Avaleht.py

--- Avaleht.py
+++ Avaleht.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
 
+st.markdown(
+    '#### Tere tulemast Nõo Reaalgümnaasiumi koolilehe Peetrike kodulehele!')
+
 st.image('https://nrgpeetrike.weebly.com/uploads/1/3/3/5/133559323/peetrikese-logo-taustata-kollane_orig.png')
-st.title("Avaleht")
-st.markdown('#### Tere tulemast Nõo Reaalgümnaasiumi koolilehe Peetrike kodulehele!')
 
 st.write("""
-         Peetrike on Nõo Reaalgümnaasiumi koolileht, mis tegeleb kooli igapäevasündmuste ning põnevamate seikade kajastamisega nii paberil kui ka veebiavarustes.
+         Koolileht, tegeleb kooli igapäevasündmuste ning põnevamate seikade kajastamisega nii paberil kui ka veebiavarustes. 
+""")
+
+st.markdown("""
+'Peetrike' on Nõo Reaalgümnaasiumi koolileht, mis on jäädvustanud meie kooli elu ja arengut aastate jooksul.
+Siin saate sirvida vanu numbreid ning avastada kooliperes toimunud olulisi hetki.
+
+Leiate palju huvitavaid lugusid, pilte ja mälestusi, mis annavad aimu meie kooli mitmekesisest ajaloost.
 """)
