7669295fb99b202bdfa8f314227acaf0c66b8880
Anri Sokolov <116002652+anriwv@users.noreply.github.com>
2024-02-17 20:59:01 +0200

    Update Kontakt.py

--- pages/Kontakt.py
+++ pages/Kontakt.py
@@ -19,7 +19,22 @@ data = {'lat': [58.2798126], 'lon': [26.5345858]}
 # Display the map using the DataFrame
 st.map(pd.DataFrame(data), zoom=13, color='#c18383')
 
-
+# HTML code with CSS for the glowing effect
+glowing_instagram_logo = """
+<style>
+.img-glow {
+  animation: glow 1s ease-in-out infinite alternate;
+}
+@keyframes glow {
+  0% {
+    filter: drop-shadow(0 0 5px #c18383);
+  }
+  100% {
+    filter: drop-shadow(0 0 15px #c18383);
+  }
+}
+</style>
+"""
 
 # Display clickable icons with specified sizes
-st.markdown("""Vaata insta kontot <a href='https://www.instagram.com/nrgpeetrike/'><img src='https://upload.wikimedia.org/wikipedia/commons/3/3e/Instagram_simple_icon.svg' width='50'></a>""", unsafe_allow_html=True)
+st.markdown("Vaata insta kontot <a href='https://www.instagram.com/nrgpeetrike/'><img src='https://github.com/anriwv/peetrike/blob/main/Instagram_logo_2022.svg.png?raw=true' width='50' class='img-glow'></a>" + glowing_instagram_logo, unsafe_allow_html=True)
